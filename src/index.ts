cb6e4e0b3ccaaa92465025b9710987c591ea3e45
Jefferson Sarti <jeffersonsarti@gmail.com>
2025-02-23 13:56:47 -0300

    chore(issues/3): Resolução de problemas de conexão entre contêiners e configuração inicial de bibliotecas e frameworks

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,32 @@ import "./util/tracing";
 import express from "express";
 import cors from "cors";
 import figlet from "figlet";
+import promBundle from "express-prom-bundle";
+import promClient from "prom-client";
+
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  includeStatusCode: true,
+  promClient: {
+    collectDefaultMetrics: {}, // Coletar métricas padrão do Prometheus, como memória e CPU
+  },
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(metricsMiddleware);
+
 app.get("/", (req, res) => {
   res.json({ message: "API Carteira Digital" });
+});
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", promClient.register.contentType); // Definir o tipo de conteúdo
+  const content = await promClient.register.metrics();
+  res.end(content); // Expor as métricas coletadas
 });
 
 app.listen(3000, () => {
